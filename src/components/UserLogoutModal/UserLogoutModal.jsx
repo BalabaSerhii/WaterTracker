7893b8b5fe57039d899a35005366114a994f4a6c
sasha0609 <oleksandralavr@gmail.com>
2024-09-05 21:34:40 +0200

    interface of modal Add, Daily Norma, Log Out

--- src/components/UserLogoutModal/UserLogoutModal.jsx
+++ src/components/UserLogoutModal/UserLogoutModal.jsx
@@ -1,7 +1,26 @@
-import css from './UserLogoutModal.module.css'
+import css from "./UserLogoutModal.module.css";
+import Modal from "../Modal/Modal";
+import css from "./logout.module.css";
+// import ButtonComponent from "../Modal/ButtonComponent/ButtonComponent";
 
-export default function UserLogoutModal() {
-    return (
-        <div className={css.container}></div>
-    )
+export default function UserLogoutModal({ onClose }) {
+  return (
+    <Modal modalTitle="Log out" onClose={onClose}>
+      <div>
+        <p className={css.text}>Do you really want to leave?</p>
+        <div className={css.div}>
+          <ButtonComponent
+            text="Cancel"
+            color="#407BFF"
+            backgroundColor="#D7E3FF"
+          />
+          <ButtonComponent
+            text="Log out"
+            color="#FFFFFF"
+            backgroundColor="#EF5050"
+          />
+        </div>
+      </div>
+    </Modal>
+  );
 }
