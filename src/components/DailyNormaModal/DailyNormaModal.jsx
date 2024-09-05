7afb9c9b81d3230e7096411e022bb96834855034
sasha0609 <oleksandralavr@gmail.com>
2024-09-05 23:38:22 +0200

    update styles

--- src/components/DailyNormaModal/DailyNormaModal.jsx
+++ src/components/DailyNormaModal/DailyNormaModal.jsx
@@ -47,97 +47,92 @@ export default function DailyNormaModal({ isOpen, onClose }) {
   if (!isOpen) return null;
 
   return (
-    <div className={css.div}>
-      <Modal modalTitle="My daily norma" onClose={onClose}>
+    <Modal modalTitle="My daily norma" onClose={onClose}>
+      <div className={css.formula_div}>
+        <p>
+          {" "}
+          For women: <span className={css.formula}>V=(M*0,03) + (T*0,4)</span>
+        </p>
+        <p>
+          {" "}
+          For men: <span className={css.formula}>V=(M*0,04) + (T*0,6)</span>
+        </p>
+      </div>
+      <p className={css.instruction_text}>
+        <span className={css.star}>* </span>V is the volume of the water norm in liters per day, M is your body
+        weight, T is the time of active sports, or another type of activity
+        commensurate in terms of loads (in the absence of these, you must set 0)
+      </p>
+      <form>
+        <p className={css.large_text}>Calculate your rate</p>
         <div className={css.formula_div}>
-          <p>
-            {" "}
-            For women :{" "}
-            <span className={css.formula}>V=(M*0,03) + (T*0,4)</span>
+          <label className={css.small_text}>
+            <input
+              type="radio"
+              value="For women"
+              checked={gender === "For women"}
+              onChange={() => setGender("For women")}
+            />
+            For women
+          </label>
+          <label className={css.small_text}>
+            <input
+              type="radio"
+              value="For man"
+              checked={gender === "For man"}
+              onChange={() => setGender("For man")}
+            />
+            For man
+          </label>
+        </div>
+        <div>
+          <p className={css.small_text}> Your weight in kilograms:</p>
+          <input
+            className={css.input}
+            type="text"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+            onBlur={calculateWaterRequirement}
+          />
+        </div>
+        <div>
+          <p className={css.small_text}>
+            The time of active participation in sports or other activities with
+            a high physical. load in hours:{" "}
           </p>
-          <p>
-            {" "}
-            For men : <span className={css.formula}>V=(M*0,04) + (T*0,6)</span>
+          <input
+            className={css.input}
+            type="text"
+            value={activityTime}
+            onChange={(e) => setActivityTime(e.target.value)}
+            onBlur={calculateWaterRequirement}
+          />
+        </div>
+        <div>
+          <p className={css.small_text}>
+            The required amount of water in liters per day:{" "}
+            <span className={css.liters}>{requiredWater}</span>
           </p>
         </div>
-        <p className={css.instruction_text}>
-          * V is the volume of the water norm in liters per day, M is your body
-          weight, T is the time of active sports, or another type of activity
-          commensurate in terms of loads (in the absence of these, you must set
-          0)
-        </p>
+        <div>
+          <p className={css.small_text}>
+            Write down how much water you will drink:{" "}
+          </p>
 
-        <form>
-          <p className={css.large_text}>Calculate your rate</p>
-          <div className={css.formula_div}>
-            <label className={css.small_text}>
-              <input
-                type="radio"
-                value="For women"
-                checked={gender === "For women"}
-                onChange={() => setGender("For women")}
-              />
-              For women
-            </label>
-            <label className={css.small_text}>
-              <input
-                type="radio"
-                value="For man"
-                checked={gender === "For man"}
-                onChange={() => setGender("For man")}
-              />
-              For man
-            </label>
-          </div>
-          <div>
-            <p className={css.small_text}> Your weight in kilograms:</p>
-            <input
-              className={css.input}
-              type="text"
-              value={weight}
-              onChange={(e) => setWeight(e.target.value)}
-              onBlur={calculateWaterRequirement}
-            />
-          </div>
-          <div>
-            <p className={css.small_text}>
-              The time of active participation in sports or other activities
-              with a high physical. load in hours:{" "}
-            </p>
-            <input
-              className={css.input}
-              type="text"
-              value={activityTime}
-              onChange={(e) => setActivityTime(e.target.value)}
-              onBlur={calculateWaterRequirement}
-            />
-          </div>
-          <div>
-            <p className={css.small_text}>
-              The required amount of water in liters per day:{" "}
-              <span className={css.liters}>{requiredWater}</span>
-            </p>
-          </div>
-          <div>
-            <p className={css.small_text}>
-              Write down how much water you will drink:{" "}
-            </p>
-
-            <input
-              className={css.input}
-              type="text"
-              value={consumedWater}
-              onChange={(e) => setConsumedWater(e.target.value)}
-            />
-          </div>
-          <button type="button" onClick={handleSave}>
-            Save
-          </button>
-          {/* <div className={css.btn_save}>
+          <input
+            className={css.input}
+            type="text"
+            value={consumedWater}
+            onChange={(e) => setConsumedWater(e.target.value)}
+          />
+        </div>
+        <button type="button" onClick={handleSave}>
+          Save
+        </button>
+        {/* <div className={css.btn_save}>
             <ButtonComponent text="Save" onClick={handleSave} />
           </div> */}
-        </form>
-      </Modal>
-    </div>
+      </form>
+    </Modal>
   );
 }
