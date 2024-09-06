fba59882a7d0b07500712d7fa4439b44bbd8f5c2
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-09-06 14:11:25 +0200

    fix icons, add custom scrollbar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
+import TodayWaterList from '../../components/TodayWaterList/TodayWaterList';
 import css from './HomePage.module.css'
 
 export default function HomePage() {
   return (
     <div className={css.container}>
+      <TodayWaterList/>
     </div>
   );
 }
