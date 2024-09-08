c6342d67d9db0cb03a1841665cabeae660e44085
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-09-08 20:19:33 +0200

    upp

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -1,7 +1,11 @@
 import css from './WaterRatioPanel.module.css'
+import ProgressBar from './ProgressBar/ProgresBar'
 
 export default function WaterRatioPanel() {
     return (
-        <div className={css.container}></div>
+        <div className={css.container}>
+            <p className={css.progresstext}>Today</p>
+            <ProgressBar/>
+        </div>
     )
 }
