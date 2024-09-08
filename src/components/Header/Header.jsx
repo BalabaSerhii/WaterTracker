005712cfd1fe72846a37e79a4ef05a97d50d346c
uliacop <115625925+uliacop@users.noreply.github.com>
2024-09-08 21:09:14 +0300

    Merge remote-tracking branch 'origin/header' into header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,5 @@
 import css from "./Header.module.css";
-import Logo from "../Logo/Logo";
-import UserAuth from "../UserAuth/UserAuth";
+
 export default function Header() {
-  return (
-    <header className={css.container}>
-      <Logo />
-      <UserAuth />
-    </header>
-  );
+  return <header className={css.container}></header>;
 }
