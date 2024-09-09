bf93bc6315895221d0279385a69b071bab74dd31
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-09-09 18:47:48 +0300

    Maksym
    modal

--- src/components/MonthStatsTable/MonthStatsTable.jsx
+++ src/components/MonthStatsTable/MonthStatsTable.jsx
@@ -1,21 +1,24 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getWaterByMonth } from '../../redux/water/operations';
 import { chosenDate as date, selectMonthly } from '../../redux/water/selectors';
 import css from './MonthStatsTable.module.css';
 import Pagitation from './Pagitation/Pagitation';
 import MonthStatsItem from './MonthStatsItem/MonthStatsItem';
+import DaysGeneralStats from '../DaysGeneralStats/DaysGeneralStats';
 
 const getMonthRange = (dateString) => {
   const date = new Date(dateString);
   const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
   const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  
   const formatDate = (d) => {
     const year = d.getFullYear();
     const month = String(d.getMonth() + 1).padStart(2, '0');
     const day = String(d.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
   };
+  
   return {
     firstDate: formatDate(firstDay),
     lastDate: formatDate(lastDay)
@@ -23,6 +26,7 @@ const getMonthRange = (dateString) => {
 };
 
 export default function MonthStatsTable() {
+  const [selectedDay, setSelectedDay] = useState(null);  
   const chosenDate = useSelector(date);
   const month = useSelector(selectMonthly);
   const dispatch = useDispatch();
@@ -33,19 +37,46 @@ export default function MonthStatsTable() {
     dispatch(getWaterByMonth(monthRange));
   }, [dispatch, monthRange]);
 
-  const dataArr = month?.data || [];
-  console.log(dataArr);
+  const handleModal = (day) => {
+    setSelectedDay(day);  
+  };
 
+  const handleCloseModal = () => {
+    setSelectedDay(null);  
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'Escape') {
+        handleCloseModal();
+      }
+    };
+
+    if (selectedDay) {
+      document.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [selectedDay]);
+  const dataArr = month?.data || [];
+  console.log(dataArr)
   return (
     <div className={css.container}>
       <Pagitation />
+      
       <ul className={css.calendar}>
         {dataArr.map((day) => (
           <li key={day.date}>
-            <MonthStatsItem day={day} />
+            <MonthStatsItem day={day} openModal={() => handleModal(day)} />
           </li>
         ))}
       </ul>
+      
+      {selectedDay && (
+        <DaysGeneralStats day={selectedDay} onClose={handleCloseModal} />
+      )}
     </div>
   );
 }
