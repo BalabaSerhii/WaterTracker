d7bc8a2b6411f9c3a20ea82c48feced0b4fbb6d3
KvashaVladyslav <146590289+KvashaVladyslav@users.noreply.github.com>
2024-09-11 12:06:21 +0200

    delete modal from comp

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -2,7 +2,7 @@ import WaterList from './WaterList'
 import css from './TodayWaterList.module.css'
 
 export default function TodayWaterList() {
-   
+    
     return (
         <div className={css.container}>
             <h2 className={css.title}>Today</h2>
