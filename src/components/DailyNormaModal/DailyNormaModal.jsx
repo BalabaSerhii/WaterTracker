4542531ff8d04dd24ad4098cf5f1bfacba852c0a
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-09-15 15:41:50 +0300

    dailyNorma

--- src/components/DailyNormaModal/DailyNormaModal.jsx
+++ src/components/DailyNormaModal/DailyNormaModal.jsx
@@ -6,23 +6,26 @@ import {
   fetchUser,
   updateUserAmountOfWater,
 } from "../../redux/user/operations";
-import { selectUserWaterAmount,selectUserData } from "../../redux/user/selectors";
-// import ButtonComponent from "../Modal/ButtonComponent/ButtonComponent";
+import { selectUserWaterAmount, selectUserData } from "../../redux/user/selectors";
 
 export default function DailyNormaModal({ isOpen, onClose, setIsOpen, setIsUpdate }) {
   const dispatch = useDispatch();
-  const userData = useSelector(selectUserData)
+  const userData = useSelector(selectUserData);
   const [gender, setGender] = useState("For women");
   const [weight, setWeight] = useState(0);
   const [activityTime, setActivityTime] = useState(0);
   const [requiredWater, setRequiredWater] = useState(1.8);
-  const [consumedWater, setConsumedWater] = useState(0);
+  const [consumedWater, setConsumedWater] = useState(requiredWater);
 
   useEffect(() => {
     if (isOpen) {
       dispatch(fetchUser());
     }
   }, [isOpen, dispatch]);
+
+  useEffect(() => {
+    setConsumedWater(requiredWater);
+  }, [requiredWater]);
 
   const weightInKg = parseFloat(weight);
   const activityInHours = parseFloat(activityTime);
@@ -36,7 +39,6 @@ export default function DailyNormaModal({ isOpen, onClose, setIsOpen, setIsUpdat
       activityInHours < 0
     ) {
       setRequiredWater(0);
-
       return;
     }
 
@@ -48,6 +50,7 @@ export default function DailyNormaModal({ isOpen, onClose, setIsOpen, setIsUpdat
 
     setRequiredWater(waterAmount.toFixed(2));
   };
+
   const handleSave = async () => {
     const obj = {
       waterAmount: Number(consumedWater),
@@ -64,19 +67,15 @@ export default function DailyNormaModal({ isOpen, onClose, setIsOpen, setIsUpdat
     <Modal modalTitle="My daily norma" onClose={onClose} setIsOpen={setIsOpen} isOpen={isOpen}>
       <div className={css.formula_div}>
         <p>
-          {" "}
           For women: <span className={css.formula}>V=(M*0,03) + (T*0,4)</span>
         </p>
         <p>
-          {" "}
           For men: <span className={css.formula}>V=(M*0,04) + (T*0,6)</span>
         </p>
       </div>
       <p className={css.instruction_text}>
         <span className={css.star}>* </span>V is the volume of the water norm in
-        liters per day, M is your body weight, T is the time of active sports,
-        or another type of activity commensurate in terms of loads (in the
-        absence of these, you must set 0)
+        liters per day, M is your body weight, T is the time of active sports, or another type of activity commensurate in terms of loads (in the absence of these, you must set 0)
       </p>
       <form>
         <p className={css.large_text}>Calculate your rate</p>
@@ -113,7 +112,7 @@ export default function DailyNormaModal({ isOpen, onClose, setIsOpen, setIsUpdat
         <div>
           <p className={css.small_text}>
             The time of active participation in sports or other activities with
-            a high physical. load in hours:{" "}
+            a high physical load in hours:{" "}
           </p>
           <input
             className={css.input}
@@ -130,23 +129,17 @@ export default function DailyNormaModal({ isOpen, onClose, setIsOpen, setIsUpdat
           </p>
         </div>
         <div>
-          <p className={css.small_text}>
-            Write down how much water you will drink:{" "}
-          </p>
-
+          <p className={css.small_text}>Write down how much water you will drink:{" "}</p>
           <input
             className={css.input}
             type="text"
-            placeholder={requiredWater}
+            value={consumedWater}
             onChange={(e) => setConsumedWater(e.target.value)}
           />
         </div>
         <button type="button" onClick={handleSave}>
           Save
         </button>
-        {/* <div className={css.btn_save}>
-            <ButtonComponent text="Save" onClick={handleSave} />
-          </div> */}
       </form>
     </Modal>
   );
